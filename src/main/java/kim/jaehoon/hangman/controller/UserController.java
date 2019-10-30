5f7d8a406cb8a0482a59a00d70e6e081669b2ea0
Jaehoon Kim <by09115@outlook.kr>
2019-10-31 00:09:19 +0900

    FIX: controller

--- src/main/java/kim/jaehoon/hangman/controller/UserController.java
+++ src/main/java/kim/jaehoon/hangman/controller/UserController.java
@@ -2,12 +2,10 @@ package kim.jaehoon.hangman.controller;
 
 import kim.jaehoon.hangman.domain.Login;
 import kim.jaehoon.hangman.domain.SignUp;
-import kim.jaehoon.hangman.domain.TokenResponse;
 import kim.jaehoon.hangman.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.validation.constraints.NotNull;
@@ -21,13 +19,18 @@ public class UserController {
 
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public Mono signUp(@RequestBody SignUp signUp) {
+    public Mono signUp(@RequestBody @NotNull SignUp signUp) {
         return userService.createUser(signUp);
     }
 
     @PostMapping("/auth")
     @ResponseStatus(HttpStatus.CREATED)
     public Mono login(@RequestBody @NotNull Login login) {
-        return userService.login(login.getEmail(), login.getPassword());
+        return userService.login(login.getId(), login.getPassword());
+    }
+
+    @GetMapping("/{id}")
+    public Mono findUserInfo(@PathVariable String id) {
+        return userService.findById(id);
     }
 }
